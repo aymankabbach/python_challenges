ca352b409f087ae3243c1762e1561f989d19fbd0
ayman <kabbachayman95@gmail.com>
2022-10-09 17:00:24 +0100

    The Weakest Rows in a Matrix

---
+++ the-weakest-rows-in-a-matrix.py
@@ -0,0 +1,70 @@
+##You are given a binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). 
+#The soldiers are positioned in front of the civilians. 
+#That is, all the 1's will appear to the left of all the 0's in each row.
+
+#A row i is weaker than a row j if one of the following is true:
+
+#The number of soldiers in row i is less than the number of soldiers in row j.
+#Both rows have the same number of soldiers and i < j.
+#Return the indices of the "number_of_output" weakest rows in the matrix ordered from weakest to strongest.
+mat=[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]]
+def read_input():
+    user_input=input("enter a number\n")
+    return user_input
+def is_input_number():
+    user_input=read_input()
+    try:
+        user_input=int(user_input)
+    except ValueError:
+        return False
+    else:
+        return user_input
+def get_number_of_output():
+    global mat
+    wrong_input=True
+    while wrong_input:
+        user_input=is_input_number()
+        if user_input==False or user_input>len(mat):
+            print("invalid input")
+        else:
+            return user_input
+def get_number_of_soldiers():
+    global mat
+    rows_soldiers=[]
+    row=0
+    for array in mat:
+        n_soldiers=0
+        for element in array:
+            if element==1:
+                n_soldiers+=1
+        rows_soldiers.append([row,n_soldiers])
+        row+=1
+    return rows_soldiers
+def get_output(rows_soldiers,array):
+    output_array=[]
+    while len(output_array)<5:
+        element_found=False
+        for element in rows_soldiers:
+            if element[1]==min(array) and element[0] not in output_array and element_found==False:
+                element_found=True
+                output_array.append(element[0])
+                array.remove(min(array))
+    return output_array
+def print_output(number_of_output,rows_soldiers,array):
+    output=get_output(rows_soldiers,array)
+    return output[:number_of_output]
+def start():
+    number_of_output=get_number_of_output()
+    rows_soldiers=get_number_of_soldiers()
+    array=[element[1] for element in rows_soldiers]
+    print(print_output(number_of_output,rows_soldiers,array))
+start()
+
+                
+
+
+
