6674be20f7668fa72a3be60fdaf0b66fd6d7b8b5
ayman <kabbachayman95@gmail.com>
2022-10-19 18:47:20 +0100

    fix some errors

--- the-weakest-rows-in-a-matrix.py
+++ the-weakest-rows-in-a-matrix.py
@@ -7,11 +7,15 @@
 #The number of soldiers in row i is less than the number of soldiers in row j.
 #Both rows have the same number of soldiers and i < j.
 #Return the indices of the "number_of_output" weakest rows in the matrix ordered from weakest to strongest.
+from matplotlib.bezier import find_bezier_t_intersecting_with_closedpath
+
+
 mat=[[1,1,0,0,0],
  [1,1,1,1,0],
  [1,0,0,0,0],
  [1,1,0,0,0],
  [1,1,1,1,1]]
+
 def read_input():
     user_input=input("enter a number\n")
     return user_input
@@ -45,14 +49,16 @@ def get_number_of_soldiers():
         row+=1
     return rows_soldiers
 def get_output(rows_soldiers,array):
+    global mat
     output_array=[]
-    while len(output_array)<5:
+    while len(output_array)<len(mat):
         element_found=False
         for element in rows_soldiers:
-            if element[1]==min(array) and element[0] not in output_array and element_found==False:
+            if element[1]==array[0] and element[0] not in output_array and element_found==False:
                 element_found=True
                 output_array.append(element[0])
-                array.remove(min(array))
+                rows_soldiers.remove(element)
+                array.pop(0)
     return output_array
 def print_output(number_of_output,rows_soldiers,array):
     output=get_output(rows_soldiers,array)
@@ -61,6 +67,7 @@ def start():
     number_of_output=get_number_of_output()
     rows_soldiers=get_number_of_soldiers()
     array=[element[1] for element in rows_soldiers]
+    array.sort()
     print(print_output(number_of_output,rows_soldiers,array))
 start()
 
