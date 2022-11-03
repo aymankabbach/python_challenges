c36a86c4e966c31a37fd6b25b5afc76fbec38d49
ayman <kabbachayman95@gmail.com>
2022-11-04 00:58:34 +0100

    syntax modification

--- challenge02.py
+++ challenge02.py
@@ -1,22 +1,16 @@
 ## City A has one million citizens and is growing at a rate of 50 thousand annually
 ## City B has 500,000 citizens and is growing 8% annually
 ## Write a program that calculates in how many years there will be more citizens in city B than in city A
-city_A_citizens=1000000
-city_B_citizens=500000
-def increase_city_A_citizens():
-    global city_A_citizens
-    city_A_citizens+=50000
-    return city_A_citizens
-def increase_city_B_citizens():
-    global city_B_citizens
-    city_B_citizens*=1.08
-    return city_B_citizens
+def increase_citizens(city_citizens,number):
+    city_citizens+=number
+    return city_citizens
 def start():
-    global city_A_citizens,city_B_citizens
+    city_A_citizens=1000000
+    city_B_citizens=500000
     year=0
     while city_B_citizens<city_A_citizens:
-        city_A_citizens=increase_city_A_citizens()
-        city_B_citizens=increase_city_B_citizens()
+        city_A_citizens=increase_citizens(city_A_citizens,50000)
+        city_B_citizens=increase_citizens(city_B_citizens,city_B_citizens*8/100)
         year+=1
     print(f"after {year} years city B would have more citizens")
 start()
